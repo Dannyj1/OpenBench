239f7e0dd295708bb2d1f45627863e6102f41b80
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2024-03-13 10:54:02 +0100

    Update utils.py

--- Client/utils.py
+++ Client/utils.py
@@ -322,7 +322,7 @@ def download_public_engine(engine, net_path, branch, source, make_path, out_path
             command.append("-G")
             command.append("Unix Makefiles")
             command.append(".")
-            process = subprocess.Popen(command, cwd=src_path, stdout=PIPE, stderr=STDOUT)
+            process = subprocess.Popen(command, cwd=make_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             cxx_output  = process.communicate()[0].decode('utf-8')
             
             # Verify that the compilation subprocess did not exit with errors
@@ -333,7 +333,7 @@ def download_public_engine(engine, net_path, branch, source, make_path, out_path
             print (cxx_output)
             
             command = "cmake --build .".split()
-            process = subprocess.Popen(command, cwd=src_path, stdout=PIPE, stderr=STDOUT)
+            process = subprocess.Popen(command, cwd=make_path, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             cxx_output = process.communicate()[0].decode('utf-8')
             
             # Verify that the compilation subprocess did not exit with errors
